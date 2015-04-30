352532674b6046e4380d735b29f42b4de4dcb5e0
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-05-01 01:48:32 +0200

    Changes in build

--- project/AmmoniteShellBuild.scala
+++ project/AmmoniteShellBuild.scala
@@ -66,6 +66,16 @@ object AmmoniteShellBuild extends Build {
     fork in (Test, testOnly) := true // Makes some tests fail in 2.10?
   )
 
+  lazy val bridge = Project(id = "bridge", base = file("bridge"))
+    .settings(sharedSettings: _*)
+    .settings(
+      name := "ammonite-bridge",
+      libraryDependencies ++= Seq(
+        "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+        "com.lihaoyi" %% "ammonite-pprint" % "0.2.7"
+      )
+    )
+
   lazy val interpreter = Project(id = "interpreter", base = file("interpreter"))
     .settings(sharedSettings: _*)
     .settings(
@@ -73,17 +83,6 @@ object AmmoniteShellBuild extends Build {
       libraryDependencies ++= Seq(
         "org.scala-lang" % "scala-compiler" % scalaVersion.value,
         "com.lihaoyi" %% "scala-parser" % "0.1.3"
-      )
-    )
-
-  lazy val bridge = Project(id = "bridge", base = file("bridge"))
-    .dependsOn(interpreter)
-    .settings(sharedSettings: _*)
-    .settings(
-      name := "ammonite-bridge",
-      libraryDependencies ++= Seq(
-        "org.scala-lang" % "scala-reflect" % scalaVersion.value,
-        "com.lihaoyi" %% "ammonite-pprint" % "0.2.7"
       )
     )
 
@@ -156,6 +155,14 @@ object AmmoniteShellBuild extends Build {
 
 
   lazy val root = Project(id = "ammonite-shell", base = file("."))
-    .aggregate(interpreter, bridge, ivyInterpreter, ivyInterpreterTests, shell, sparkBridge, pprintShapeless, sparkIvyInterpreter, sparkShell)
+    .settings(sharedSettings: _*)
+    .aggregate(bridge, interpreter, ivyInterpreter, ivyInterpreterTests, shell, sparkBridge, pprintShapeless, sparkIvyInterpreter, sparkShell)
+    .dependsOn(bridge, interpreter, ivyInterpreter, ivyInterpreterTests, shell, sparkBridge, pprintShapeless, sparkIvyInterpreter, sparkShell)
+    .settings(
+      publish := {},
+      publishLocal := {},
+      (unmanagedSourceDirectories in Compile) := Nil,
+      (unmanagedSourceDirectories in Test) := Nil
+    )
 
 }
