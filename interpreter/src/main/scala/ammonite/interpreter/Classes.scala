b5489ea6417272f38bd05ea828c5468466cb07af
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-05-01 01:48:33 +0200

    Moved back ClassesLazilyMaterialize to interpreter

--- interpreter/src/main/scala/ammonite/interpreter/Classes.scala
+++ interpreter/src/main/scala/ammonite/interpreter/Classes.scala
@@ -1,7 +1,8 @@
 package ammonite.interpreter
 
-import java.io.File
+import java.io.{FileOutputStream, File}
 import java.net.{URLClassLoader, URL}
+import java.util.UUID
 
 trait Classes {
   def currentClassLoader: ClassLoader
@@ -96,5 +97,39 @@ class DefaultClassesImpl(
   var onJarsAddedHooks = Seq.empty[Seq[File] => Unit]
   def onJarsAdded(action: Seq[File] => Unit) = {
     onJarsAddedHooks = onJarsAddedHooks :+ action
+  }
+}
+
+trait ClassesLazilyMaterialize extends Classes {
+  lazy val tmpClassDir = {
+    val d = new File(new File(System.getProperty("java.io.tmpdir")), s"ammonite-${UUID.randomUUID()}")
+    d.mkdirs()
+    d.deleteOnExit()
+    d
+  }
+
+  var overriddenClassLoader: ClassLoader = _
+
+  abstract override def currentClassLoader: ClassLoader = {
+    val _super = super.currentClassLoader
+    if (overriddenClassLoader == null || overriddenClassLoader.getParent != _super)
+      overriddenClassLoader = new URLClassLoader(Array(), _super) {
+        // FIXME getResources should be overloaded too
+        override def getResource(name: String) =
+          Some(name).filter(_ endsWith ".class").map(_ stripSuffix ".class").flatMap(fromClassMaps) match {
+            case Some(bytes) =>
+              val f = new File(tmpClassDir, name)
+              if (!f.exists()) {
+                val w = new FileOutputStream(f)
+                w.write(bytes)
+                w.close()
+              }
+              f.toURI.toURL
+            case None =>
+              super.getResource(name)
+          }
+      }
+
+    overriddenClassLoader
   }
 }
