659a68d96fcce8ba114e76475f255996f6fa89d8
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-01-28 19:29:53 +0100

    Add launchers

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-VERSION=0.4.0-SNAPSHOT
+VERSION=0.4.0-M1
+
+if [ "$1" = "--2.10" ]; then
+  SCALA_VERSION=2.10.6
+  OUTPUT=ammonium-2.10
+else
+  SCALA_VERSION=2.11.7
+  OUTPUT=ammonium
+fi
 
 "$(dirname "$0")/../coursier" bootstrap \
-  com.github.alexarchambault.ammonium:shell_2.11.7:$VERSION \
-  -I ammonium-compile:com.github.alexarchambault.ammonium:shell-api_2.11.7:$VERSION \
-  -I ammonium-macro:org.scala-lang:scala-compiler:2.11.7 \
+  com.github.alexarchambault.ammonium:shell_$SCALA_VERSION:$VERSION \
+  -I ammonium-compile:com.github.alexarchambault.ammonium:shell-api_$SCALA_VERSION:$VERSION \
+  -I ammonium-macro:org.scala-lang:scala-compiler:$SCALA_VERSION \
   -i ammonium-compile,ammonium-macro \
+  --no-default \
   -r central \
+  -r sonatype:releases \
   -D "\${user.home}/.ammonium/bootstrap/$VERSION" \
-  -f -o ammonium \
+  -f -o "$OUTPUT" \
   -M ammonite.Ammonite
