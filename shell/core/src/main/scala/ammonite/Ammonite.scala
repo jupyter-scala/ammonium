756b8fb73101fd3ee762dbb8d2a3202f8434f9fc
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-01-28 19:35:29 +0100

    Add Sonatype releases repo by default

--- shell/core/src/main/scala/ammonite/Ammonite.scala
+++ shell/core/src/main/scala/ammonite/Ammonite.scala
@@ -136,7 +136,8 @@ object Ammonite extends AppOf[Ammonite] {
 
   val initialRepositories = Seq(
     coursier.Cache.ivy2Local,
-    MavenRepository("https://repo1.maven.org/maven2")
+    MavenRepository("https://repo1.maven.org/maven2"),
+    MavenRepository("https://oss.sonatype.org/content/repositories/releases")
   ) ++ {
     if (BuildInfo.version.endsWith("-SNAPSHOT")) Seq(
       MavenRepository("https://oss.sonatype.org/content/repositories/snapshots")
