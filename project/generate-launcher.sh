c4047d15ade2bfb7739680fa7f319458da8aa447
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-02-03 09:46:22 +0100

    Fix possible issue with 2.10 launcher

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -6,6 +6,7 @@ if [ "$1" = "--2.10" ]; then
   SCALA_VERSION=2.10.6
   OUTPUT=ammonium-2.10
   EXTRA="
+    -E com.chuusai:shapeless_2.10.4 \
     -I ammonium-compile:org.scala-lang:scala-compiler:$SCALA_VERSION \
     -I ammonium-compile:org.scalamacros:quasiquotes_2.10:2.0.1 \
   "
