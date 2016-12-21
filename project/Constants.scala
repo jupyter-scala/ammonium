e8f515d059ae37011b3363d608f4cc6596ad39a7
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-12-21 10:39:12 +0100

    [fork] Set version to 0.8.2-SNAPSHOT

--- project/Constants.scala
+++ project/Constants.scala
@@ -1,7 +1,7 @@
 package ammonite
 
 object Constants{
-  val version = "0.8.1"
+  val version = "0.8.2-SNAPSHOT"
   val unstableVersion = "unstableVersion-SNAPSHOT"
   val curlUrl = "<curlUrl-set-me-in-Constants.scala>"
   val unstableCurlUrl = "<unstableCurlUrl-set-me-in-Constants.scala>"
